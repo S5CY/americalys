e0d558beda21f7c9570ef586c2e8de5ca76847fd
S5CY <merylchensd@gmail.com>
2026-09-03 12:05:40 -0700

    Connect attendance service

--- assets/js/attendance-config.js
+++ assets/js/attendance-config.js
@@ -1,2 +1,1 @@
-// Paste the deployed Google Apps Script /exec URL here after deployment.
-window.AMERICALYS_ATTENDANCE_API = "";
+window.AMERICALYS_ATTENDANCE_API = "https://script.google.com/macros/s/AKfycbycRjYIoFYQcB95ewWuyz6f6w91v7n8-fAMODkBJL3U_keCFYU51-nTo9bpjqO2oNSb/exec";
